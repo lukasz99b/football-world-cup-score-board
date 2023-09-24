b9f78e7d9fe0b32e5ff7e01ed6747fecde5f44a7
Łukasz Bacik <mail@luka.sh>
2023-09-24 17:58:05 +0200

    Acceptance test implementation (behat)

--- features/bootstrap/FeatureContext.php
+++ features/bootstrap/FeatureContext.php
@@ -1,5 +1,12 @@
 <?php
 
+declare(strict_types=1);
+
+namespace Features;
+
+use App\Infrastructure\InMemoryRepository;
+use App\ScoreBoard\ScoreBoard;
+use App\ScoreBoard\SummaryReportGenerator\Simple;
 use Behat\Behat\Context\Context;
 use Behat\Gherkin\Node\PyStringNode;
 use Behat\Gherkin\Node\TableNode;
@@ -9,14 +16,42 @@ use Behat\Gherkin\Node\TableNode;
  */
 class FeatureContext implements Context
 {
-    /**
-     * Initializes context.
-     *
-     * Every scenario gets its own context instance.
-     * You can also pass arbitrary arguments to the
-     * context constructor through behat.yml.
-     */
+    private ScoreBoard $scoreBoard;
+    private string $report;
+
     public function __construct()
     {
+        $this->scoreBoard = new ScoreBoard(
+            new InMemoryRepository(),
+            new Simple(),
+        );
+    }
+
+    /**
+     * @Given /^the following matches were already played:$/
+     */
+    public function theFollowingMatchesWerePlayed(TableNode $table)
+    {
+        foreach ($table as $row) {
+            $this->scoreBoard->startGame($row['Home team'], $row['Away team']);
+            $this->scoreBoard->updateScore((int) $row['Home goals'], (int) $row['Away goals']);
+            $this->scoreBoard->finishGame();
+        }
+    }
+
+    /**
+     * @When /^I ask for the summary report$/
+     */
+    public function iAskForTheSummaryReport()
+    {
+        $this->report = $this->scoreBoard->getFinishedGamesSummary();
+    }
+
+    /**
+     * @Then /^I should receive$/
+     */
+    public function iShouldReceive(PyStringNode $string)
+    {
+        assert(trim($this->report) === $string->getRaw());
     }
 }
